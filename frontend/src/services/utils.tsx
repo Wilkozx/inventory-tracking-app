529e303863559609cfc8128164a98b3151f1a975
Wilkozx <wilkoisontheceiling@gmail.com>
2025-01-19 00:26:15 +0000

    + variable search limit
    default 20

--- frontend/src/services/utils.tsx
+++ frontend/src/services/utils.tsx
@@ -3,12 +3,13 @@ export async function getSearch(
     query: string = "",
     sort_by: string = "",
     page: number = 0,
+    limit: number = 20,
 ) {
     let response;
     if (query) {
-        response = await fetch("/api/" + target + "/?filter_by=" + query + "&sort_by=" + sort_by + "&limit=20" + "&page=" + page)
+        response = await fetch("/api/" + target + "/?filter_by=" + query + "&sort_by=" + sort_by + "&limit=" + limit + "&page=" + page)
     } else {
-        response = await fetch("/api/" + target + "/?sort_by=" + sort_by + "&limit=20" + "&page=" + page)
+        response = await fetch("/api/" + target + "/?sort_by=" + sort_by + "&limit=" + limit + "&page=" + page)
     }
 
     let data = await response.json()
