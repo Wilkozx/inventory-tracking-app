8d042e37789473b0a49b2b24c732452f94783d33
Wilkozx <wilkoisontheceiling@gmail.com>
2025-01-21 00:03:54 +0000

    + post request function

--- frontend/src/services/utils.tsx
+++ frontend/src/services/utils.tsx
@@ -15,3 +15,17 @@ export async function getSearch(
     let data = await response.json()
     return data
 }
+
+export async function postRequest(
+    target: string = "",
+    payload: {} = {}
+) {
+    let response;
+    response = await fetch("/api/" + target + "", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+}
